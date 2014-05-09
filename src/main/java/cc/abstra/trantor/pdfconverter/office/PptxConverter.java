8abab9c57c8ea8482dbeab6fd88ba664de97fd1e
Oscar Lara <oscar@abstra.cc>
2014-05-09 14:00:58 +0200

    Added converters for xls, xlsx, ppt and pptx

---
+++ src/main/java/cc/abstra/trantor/pdfconverter/office/PptxConverter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cc.abstra.trantor.pdfconverter.office;
+
+public class PptxConverter extends OfficeDocConverter{
+    public PptxConverter(){}
+    public PptxConverter(String OfficeDocPath) {
+        super();
+    }
+}
---
+++ src/main/java/cc/abstra/trantor/pdfconverter/office/XlsConverter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cc.abstra.trantor.pdfconverter.office;
+
+public class XlsConverter extends OfficeDocConverter{
+    public XlsConverter(){}
+    public XlsConverter(String OfficeDocPath) {
+        super();
+    }
+}
